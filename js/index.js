ed080c43f5a055805402ec0aee8655b6e1442132
MattiasMR <m.moralesr@udd.cl>
2024-10-24 22:37:43 -0300

    change url

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
-
-  document.addEventListener('DOMContentLoaded', function() {
-    const aboutLink = document.getElementById('aboutLink');
-    aboutLink.addEventListener('click', function(event) {
-      event.preventDefault(); // Evita que el enlace navegue directamente
-      window.location.href = 'assets/ingles.pdf'; // Redirige al PDF
-    });
+document.addEventListener('DOMContentLoaded', function() {
+  const aboutLink = document.getElementById('aboutLink');
+  aboutLink.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default anchor behavior
+    // Change the URL without reloading the page
+    window.history.pushState({}, '', '#about_us');
+    // Redirect to the PDF file
+    window.location.href = 'assets/ingles.pdf';
   });
+});
