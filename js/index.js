d0a1078184a906c1156f5a05e30b257399c79625
MattiasMR <m.moralesr@udd.cl>
2024-10-28 17:18:00 -0300

    fixing pdfs

--- js/index.js
+++ js/index.js
@@ -1,13 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const aboutLink = document.getElementById('aboutLink');
-  aboutLink.addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent the default anchor behavior
-    // Change the URL without reloading the page
-    window.history.pushState({}, '', '#about_us');
-    // Redirect to the PDF file
-    window.location.href = 'assets/ingles.pdf';
-  });
-});
+function loadBr() {
+  // Load the 'about.html' page content (assuming AJAX or similar for dynamic loading)
+  window.location.href = 'br.html'; // Replace with actual page loading mechanism
+
+  // Change the URL to a masked version
+  history.pushState(null, null, "/sobre-nos"); // Change '/sobre-nos' to your preferred path
+}
+
+function loadEn() {
+  // Load the 'about.html' page content (assuming AJAX or similar for dynamic loading)
+  window.location.href = 'en.html'; // Replace with actual page loading mechanism
+
+  // Change the URL to a masked version
+  history.pushState(null, null, "/about-us"); // Change '/sobre-nos' to your preferred path
+}
+
+function loadEs() {
+  // Load the 'about.html' page content (assuming AJAX or similar for dynamic loading)
+  window.location.href = 'es.html'; // Replace with actual page loading mechanism
+
+  // Change the URL to a masked version
+  history.pushState(null, null, "/sobre-nosotros"); // Change '/sobre-nos' to your preferred path
+}
 
 function setLanguage(flag) {
   // Change the displayed flag to the selected language's flag
