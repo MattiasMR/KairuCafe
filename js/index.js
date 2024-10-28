e113e3b8d90f4b71e8af283b6e2d624fc6218ee1
MattiasMR <m.moralesr@udd.cl>
2024-10-28 17:31:25 -0300

    masking url

--- js/index.js
+++ js/index.js
@@ -1,25 +1,16 @@
 function loadBr() {
   // Load the 'about.html' page content (assuming AJAX or similar for dynamic loading)
   window.location.href = 'br.html'; // Replace with actual page loading mechanism
-
-  // Change the URL to a masked version
-  history.pushState(null, null, "/sobre-nos"); // Change '/sobre-nos' to your preferred path
 }
 
 function loadEn() {
   // Load the 'about.html' page content (assuming AJAX or similar for dynamic loading)
   window.location.href = 'en.html'; // Replace with actual page loading mechanism
-
-  // Change the URL to a masked version
-  history.pushState(null, null, "/about-us"); // Change '/sobre-nos' to your preferred path
 }
 
 function loadEs() {
   // Load the 'about.html' page content (assuming AJAX or similar for dynamic loading)
   window.location.href = 'es.html'; // Replace with actual page loading mechanism
-
-  // Change the URL to a masked version
-  history.pushState(null, null, "/sobre-nosotros"); // Change '/sobre-nos' to your preferred path
 }
 
 function setLanguage(flag) {
